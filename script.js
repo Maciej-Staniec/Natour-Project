530f7d974a574d9d418e8f5c46efc57f52dbc571
Maciej Staniec <maciej.staniec@yahoo.co.uk>
2024-04-30 20:36:17 +0100

    commit

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-window.addEventListener("resize", function () {
-  const gridItems = document.querySelectorAll(".gridItem"); // Select all elements with class 'gridItem'
-  const screenWidth = window.innerWidth;
 
-  gridItems.forEach(function (gridItem) {
-    if (screenWidth <= 900) {
-      gridItem.classList.remove("col-1-of-4");
-      gridItem.classList.add("col-1-of-2");
-    } else {
-      gridItem.classList.remove("col-1-of-2");
-      gridItem.classList.add("col-1-of-4");
-    }
-  });
-});
