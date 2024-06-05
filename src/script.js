e7fcfd2480b8a3e52aee35003322734f64759477
Maciej Staniec <maciej.staniec@yahoo.co.uk>
2024-06-05 08:56:15 +0100

    commit

--- src/script.js
+++ src/script.js
@@ -1,5 +1,15 @@
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/all";
+
+gsap.registerPlugin(ScrollToPlugin);
+
+// *********** FUNCTIONS ***********
+function scrollToSection(sectionId) {
+  gsap.to(window, { duration: 1, scrollTo: sectionID });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Responsive video
+  // *********** Responsive video functionality ***********
   const video = document.querySelector(".bg-video__content");
 
   if (video) {
@@ -24,18 +34,33 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     console.error("Video element not found");
   }
-  // Navigation functionality
 
-  // Close the nav menu if one of those links is clicked
+  // *********** Navigation functionality ***********
+
   const navLinks = document.querySelectorAll(".navigation__link");
   console.log(navLinks);
   navLinks.forEach(function (link) {
+    // Add a click event listener to each link.
     link.addEventListener("click", function () {
+      // Prevent the default behavior of the link, which is to jump to the section instantly.
+      event.preventDefault();
+
+      // ====== Popup close functionality ======
+
+      // Get the input element of the checkbox type, which is the element with a navigation__checkbox class.
       const navCheckbox = document.querySelector(".navigation__checkbox");
-      console.log("Clicked");
+      // Close the Navigation when a nav link is clicked
       navCheckbox.checked = false;
+      console.log("Clicked");
+
+      // ====== SMOOTH SCROLL SUPPORT ======
+      // Smooth scrolling to sections using GSAP package
+
+      // Get the href attribute of the clicked link, which contains the ID of the target section.
+      const targetId = this.getAttribute("href");
+
+      // Call the scrollToSection function with the target ID to perform the smooth scroll.
+      scrollToSection(targetId);
     });
   });
-
-  // Popup close functionality
 });
