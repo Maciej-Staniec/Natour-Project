a5fcf9ad267bc10f2969c13dbc89b0cf775776d9
Maciej Staniec <maciej.staniec@yahoo.co.uk>
2024-06-06 18:13:06 +0100

    end of development

--- dist/script.compiled.js
+++ dist/script.compiled.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",function(){var e,o=document.querySelector(".bg-video__content");o?(e=window.innerWidth,o.innerHTML=e<=900?`
+document.addEventListener("DOMContentLoaded",function(){gsap.registerPlugin(ScrollToPlugin);var e,o=document.querySelector(".bg-video__content");o?(e=window.innerWidth,o.innerHTML=e<=900?`
         <source src="img/video-tablet.mp4" type="video/mp4">
         <source src="img/video-tablet.webm" type="video/webm">
         Your browser is not supported!
@@ -6,4 +6,4 @@ document.addEventListener("DOMContentLoaded",function(){var e,o=document.querySe
         <source src="img/video.mp4" type="video/mp4">
         <source src="img/video.webm" type="video/webm">
         Your browser is not supported!
-      `,o.load()):console.error("Video element not found")});
+      `,o.load()):console.error("Video element not found"),document.querySelectorAll(".navigation__link").forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();document.querySelector(".navigation__checkbox").checked=!1;e=this.getAttribute("href");gsap.to(window,{duration:2,scrollTo:e})})})});
--- src/script.js
+++ src/script.js
@@ -1,28 +1,27 @@
-import { gsap } from "gsap";
-import { ScrollToPlugin } from "gsap/all";
-
-gsap.registerPlugin(ScrollToPlugin);
-
-// *********** FUNCTIONS ***********
-function scrollToSection(sectionId) {
-  gsap.to(window, { duration: 1, scrollTo: sectionID });
-}
-
 document.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollToPlugin);
+
+  // *********** FUNCTIONS ***********
+
+  // Scroll to a section
+  function scrollToSection(sectionId) {
+    gsap.to(window, { duration: 2, scrollTo: sectionId });
+  }
+
   // *********** Responsive video functionality ***********
   const video = document.querySelector(".bg-video__content");
 
   if (video) {
     const width = window.innerWidth;
     if (width <= 900) {
-      // Tablet
+      // Tablet size
       video.innerHTML = `
         <source src="img/video-tablet.mp4" type="video/mp4">
         <source src="img/video-tablet.webm" type="video/webm">
         Your browser is not supported!
       `;
     } else {
-      // Desktop
+      // Desktop size
       video.innerHTML = `
         <source src="img/video.mp4" type="video/mp4">
         <source src="img/video.webm" type="video/webm">
@@ -30,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
       `;
     }
 
-    video.load(); // Reload the video with new sources
+    video.load(); // load the video based on the screen width
   } else {
     console.error("Video element not found");
   }
@@ -38,10 +37,9 @@ document.addEventListener("DOMContentLoaded", function () {
   // *********** Navigation functionality ***********
 
   const navLinks = document.querySelectorAll(".navigation__link");
-  console.log(navLinks);
   navLinks.forEach(function (link) {
     // Add a click event listener to each link.
-    link.addEventListener("click", function () {
+    link.addEventListener("click", function (event) {
       // Prevent the default behavior of the link, which is to jump to the section instantly.
       event.preventDefault();
 
@@ -51,13 +49,14 @@ document.addEventListener("DOMContentLoaded", function () {
       const navCheckbox = document.querySelector(".navigation__checkbox");
       // Close the Navigation when a nav link is clicked
       navCheckbox.checked = false;
-      console.log("Clicked");
+      // console.log("Clicked");
 
       // ====== SMOOTH SCROLL SUPPORT ======
       // Smooth scrolling to sections using GSAP package
 
       // Get the href attribute of the clicked link, which contains the ID of the target section.
       const targetId = this.getAttribute("href");
+      // console.log(targetId);
 
       // Call the scrollToSection function with the target ID to perform the smooth scroll.
       scrollToSection(targetId);
